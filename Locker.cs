2e6e3b0790996f769b342c5f662b193f6d91ae7d
peroxidee <iluvwerewolves333@gmail.com>
2024-07-13 01:35:35 -0700

    finished it up

--- Locker.cs
+++ Locker.cs
@@ -1,11 +1,24 @@
-﻿System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
+
 
 namespace gigas;
 
 public class Locker
 {
+
+
     public static void Pos()
     {
-        
+        while (1==1)
+        {
+            Console.SetCursorPosition(0,0 );
+            if (Console.ReadKey().Key != ConsoleKey.K && Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                break;
+            }
+            
+        }
     }
+    
 }
